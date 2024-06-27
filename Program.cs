a0f9be24a82555558c96f2de2afc3e6f82a031c5
Khusandev <husannabiev44@gmail.com>
2024-06-27 15:02:54 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace praktikum_task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("radiusni kiriting (input):");
+            string radius = Console.ReadLine();
+           int rad = Convert.ToInt32(radius);
+
+            double S = Math.PI * (rad* rad);
+            double L = 2*Math.PI * rad;
+
+            Console.WriteLine($"Doira yuzasi(output):{S}");
+            Console.WriteLine($"Aylana uzunligi (output):{L}");
+           
+
+            
+        }
+    }
+}
